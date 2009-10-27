682110901d5c35c22e40f297a5335c9f8d9d1476
Blythe Dunham <blythedunham@snowgiraffe-super-funtime.local>
2009-10-27 14:41:51 -0700

    Use 32 bit mongo config and disable mongomapper gem install

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -16,16 +16,27 @@ directory "/data/slave" do
   mode 0755
   recursive true
 end
-  
+
 execute "install-mongodb" do
   command %Q{
-    curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-1.0.0.tgz &&
-    tar zxvf mongodb-linux-x86_64-1.0.0.tgz &&
-    mv mongodb-linux-x86_64-1.0.0 /usr/local/mongodb &&
-    rm mongodb-linux-x86_64-1.0.0.tgz
+    curl -O http://downloads.mongodb.org/linux/mongodb-linux-i686-1.0.1.tgz &&
+    tar zxvf mongodb-linux-i686-1.0.1.tgz &&
+    mv mongodb-linux-i686-1.0.1 /usr/local/mongodb &&
+    rm mongodb-linux-i686-1.0.1.tgz
   }
   not_if { File.directory?('/usr/local/mongodb') }
 end
+
+#execute "install-mongodb-64" do
+
+#  command %Q{
+#    curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-1.0.0.tgz &&
+#    tar zxvf mongodb-linux-x86_64-1.0.0.tgz &&
+#    mv mongodb-linux-x86_64-1.0.0 /usr/local/mongodb &&
+#    rm mongodb-linux-x86_64-1.0.0.tgz
+#  }
+#  not_if { File.directory?('/usr/local/mongodb') }
+#end
   
 execute "add-to-path" do
   command %Q{
@@ -34,11 +45,11 @@ execute "add-to-path" do
   not_if "grep 'export PATH=$PATH:/usr/local/mongodb/bin' /etc/profile"
 end
   
-execute "install-mongomapper" do
-  command %Q{
-    gem install jnunemaker-mongomapper --source http://gems.github.com
-  }
-end
+#execute "install-mongomapper" do
+#  command %Q{
+#    gem install jnunemaker-mongomapper --source http://gems.github.com
+#  }
+#end
 
 remote_file "/etc/init.d/mongodb" do
   source "mongodb"
